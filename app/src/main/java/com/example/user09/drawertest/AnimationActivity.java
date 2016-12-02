9f911653d9fed9627da50560b80a67c05427dcc6
EmeraldWolfram <stevenfgy@yahoo.com>
2016-12-02 21:16:57 +0800

    Added Animation for swiping

--- app/src/main/java/com/example/user09/drawertest/AnimationActivity.java
+++ app/src/main/java/com/example/user09/drawertest/AnimationActivity.java
@@ -1,12 +1,17 @@
 package com.example.user09.drawertest;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class AnimationActivity extends AppCompatActivity {
 
@@ -37,6 +42,101 @@ public class AnimationActivity extends AppCompatActivity {
         enterFromRight  = AnimationUtils.loadAnimation(AnimationActivity.this, R.anim.entering_anim_right_in_1);
         exitToRight     = AnimationUtils.loadAnimation(AnimationActivity.this, R.anim.exiting_anim_right_out);
         containerLayout = (LinearLayout) findViewById(R.id.childLayout);
+
+        enterFromRight.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                containerLayout.animate().translationX(0).setDuration(300).start();
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+        });
+        enterAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                containerLayout.animate().translationX(0).setDuration(300).start();
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+        });
+        exitToRight.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                containerLayout.startAnimation(enterAnim);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        exitAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                containerLayout.startAnimation(enterFromRight);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        containerLayout.setOnTouchListener(new View.OnTouchListener() {
+            float dX;
+            float startingX, endingX, viewChanges;
+
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        startingX = view.getX();
+                        dX = startingX - event.getRawX();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        view.animate().x(event.getRawX() + dX).setDuration(0).start();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        endingX     = view.getX();
+                        viewChanges = endingX - startingX;
+                        if(viewChanges > 300){
+                            view.startAnimation(exitToRight);
+                        } else if(viewChanges < -300) {
+                            view.startAnimation(exitAnim);
+                        } else {
+                            view.animate().translationX(0).setDuration(300).start();
+                        }
+                        break;
+                    default:
+                        return false;
+                }
+                return true;
+            }
+        });
+
     }
 
 
